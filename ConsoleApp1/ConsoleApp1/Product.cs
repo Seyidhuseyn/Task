fa2342d34eba912204b7d97fb42043e0c51c94e9
AliMusa04 <tu75NX176@code.edu.az>
2022-11-24 08:43:25 +0400

    homework

---
+++ ConsoleApp1/ConsoleApp1/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Product
+    {
+        private string Name;
+        private int Price;
+
+        public Product(string name, int price)
+        {
+            string Name = name;
+            double Price = price;
+        }
+        private string Name
+        {
+            get { return name; }
+            set 
+            {
+                if (name.Length>=6 && name.Length<=25)
+                {
+                name = value; 
+                }
+                else
+                {
+                    Console.WriteLine("Adi duzgun daxil edin");
+                }
+            }
+        }
+
+
+        public static bool CheckPassword(string name, int price)
+        {
+            for (int i = 0; i < name.Length; i++)
+            {
+                if ()
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/User.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class User
+    {
+        private string username;
+        private int password;
+        private string issignedin;
+        private string[] products;
+
+
+        public User(string username, int password, string issignedin, string[] product)
+        {
+            string Username = username;
+            int Password = password;
+            string IsSignedIn = issignedin;
+            string[] Products = products;
+        }
+
+        private string Username
+        {
+            get { return username; }
+            set
+            {
+                if (username.Length >= 6 && username.Length <= 25)
+                {
+                    username = value;
+                }
+                else
+                {
+                    Console.WriteLine("Adi duzgun daxil edin");
+                }
+            }
+        }
+
+        private int Password
+        {
+            get { return password; }
+            set
+            {
+                if (password.Length >= 8 && password.Length <= 25)
+                {
+                    password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Sifreni duzgun daxil edin");
+                }
+            }
+        }
+    }
+}
